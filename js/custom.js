bc798a98a5d436c23034618ecd0ea12c3ce072c4
Brad Tracy <bradktracy@gmail.com>
2015-02-07 11:53:50 -0500

    Primary Sync

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+/*
+My Custom JS
+Updated 12/28/2014
+*/
+
+$(function() {
+
+	$('#alertme').click(function(e)	{
+
+		e.preventDefault();
+
+		$('#successAlert').slideDown();
+
+	});
+
+
+});
+
+
+(jQuery);
+
+/* ========================================================================
+ * Bootstrap: transition.js v3.3.1
+ * http://getbootstrap.com/javascript/#transitions
+ * ========================================================================
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
+
+ 
